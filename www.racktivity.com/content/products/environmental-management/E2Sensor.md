ae4d693b86e62571aa247ba9e1886243f566fe4c
Fady Maged <magedf@codescalers.com>
2016-02-03 17:35:57 +0200

    update the whole website

--- www.racktivity.com/content/products/environmental-management/E2Sensor.md
+++ www.racktivity.com/content/products/environmental-management/E2Sensor.md
@@ -1,7 +1,7 @@
 +++
 title= "E²Sensor"
 page_disc = "Racktivity developed the E²Sensor Family to give you visibility of the environmental situation of your data centers and remote infrastructure."
-section_pic = "/images/product-banner3"
+section_pic = "/images/product-banner3.jpg"
 parent = "environmental-management"
 real_link = "E2Sensor"
 image = "/images/products_energysensor_gallery1.png"
--- www.racktivity.com/themes/landing/static/js/general.js
+++ www.racktivity.com/themes/landing/static/js/general.js
@@ -1,3 +1,4 @@
  var str=location.href.toLowerCase();
     $('li.active').removeClass("active");
-  $('.nav li a').filter(function() {return this.href.toLowerCase() == str; }).parents('li').addClass('active');
+	$('.nav li a').filter(function() {return this.href.toLowerCase() == str; }).parents('li').addClass('active');
+    $('.section-side li a ').filter(function() {return this.href.toLowerCase() == str; }).addClass('active');

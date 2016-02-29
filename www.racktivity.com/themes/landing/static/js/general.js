42ecbcca9180ce376bf061d213c3d12e8f8773f2
Fady Maged <magedf@codescalers.com>
2016-02-29 18:24:47 +0200

    Add search input on the home page

--- www.racktivity.com/themes/landing/static/js/general.js
+++ www.racktivity.com/themes/landing/static/js/general.js
@@ -1,11 +1,30 @@
- var str=location.href.toLowerCase();
+$( document ).ready(function() {
+
+var str=location.href.toLowerCase();
     $('li.active').removeClass("active");
 	$('.nav li a').filter(function() {return this.href.toLowerCase() == str; }).parents('li').addClass('active');
     $('.section-side li a ').filter(function() {return this.href.toLowerCase() == str; }).addClass('active');
-
- 
  $('.carousel').carousel({
  	interval: 10000,
- 	pause: 'hover'
- 	
+ 	pause: 'hover' 	
 })
+
+$(".search-box").keyup(function(event){
+    if(event.keyCode == 13){
+        $(".search-icon").click();
+    }
+});
+	
+   $(".search-icon").click(function () { 
+	var searchValue = $('.search-box').val(); 
+   	window.location.href = "/search" + "#" + searchValue;   	  
+	});
+
+   $('#search').val(window.location.href.split('#')[1]);
+
+	
+
+
+
+
+});
